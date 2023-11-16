07c46dd2c8e2b2be8bf4474f64a3ac2d6e51e604
Tom <contact@jsdevtom.com>
2023-11-16 13:02:59 +0000

    test(app): test calculator

--- apps/nx-wallaby/src/app/calculator/calculator.component.spec.ts
+++ apps/nx-wallaby/src/app/calculator/calculator.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalculatorComponent } from './calculator.component';
+import { screen } from '@testing-library/dom';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
@@ -17,5 +18,25 @@ describe('CalculatorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show 0 as initial value', () => {
+    expect(screen.getByTestId('count').textContent!.trim()).toBe('0');
+  });
+
+  it('should increment the value', () => {
+    screen.getByTestId('increment-button').click();
+    screen.getByTestId('increment-button').click();
+    fixture.detectChanges();
+    expect(screen.getByTestId('count').textContent!.trim()).toBe('2');
+    expect(screen.getByTestId('product').textContent!.trim()).toBe('4');
+  });
+
+  it('should decrement the value', () => {
+    screen.getByTestId('decrement-button').click();
+    screen.getByTestId('decrement-button').click();
+    fixture.detectChanges();
+    expect(screen.getByTestId('count').textContent!.trim()).toBe('-2');
+    expect(screen.getByTestId('product').textContent!.trim()).toBe('-4');
   });
 });
